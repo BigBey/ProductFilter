674d28e624ef6728a494b81aa07ed0f89ad16216
BigBey <bey.sviatoslav@yandex.ru>
2019-12-08 04:24:27 +0300

    some changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,9 +15,13 @@ public class Main {
         productList.add(new Product(45d, "black", "45", ProductType.SOCKS));
         productList.add(new Product(43d, "red", "s", ProductType.T_SHIRT));
         productList.add(new Product(45d, "red", "s", ProductType.T_SHIRT));
-        BaseFilter filter = new PriceFilter(new Pair<>(40d, 45d));
+        BaseFilter filter = new PriceFilter(new Pair<>(40d, 50d));
         filter.addFilter(new ColorFilter("red"));
         //sizeFilter.setProductList(productList);
-        System.out.println(filter.find(productList).toString());
+        try {
+            System.out.println(filter.find(productList).toString());
+        }catch (NullPointerException e){
+            System.out.println("Товаров не найдено");
+        }
     }
 }
